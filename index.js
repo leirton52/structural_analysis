6f30c28ce3c5bcefe4491d8d9a68dc513ae39f0f
Leirton Freitas <mttomleirton@gmail.com>
2021-05-04 20:02:53 -0300

    testando criar o servidor com node, utilizando express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const http = require('http')
+
+const app = express()
+const server = http.createServer(app)
+
+app.use(express.static('src'))
+
+server.listen(3000, () => {
+    console.log(`> Server listening on port: 3000`)
+})
+
