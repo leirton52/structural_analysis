111e4330f37f8306ba5c6970b1853a100eb3b267
Leirton Freitas <mttomleirton@gmail.com>
2021-11-03 23:12:13 -0300

    finalizando a função que monta a matriz de rigidez global

--- src/modules/matrizRigidezGlobal.js
+++ src/modules/matrizRigidezGlobal.js
@@ -1,14 +1,35 @@
 const mntMatrizRigidezGlobal = (nodes, lines) => {
     //obtendo a tamanho da matriz de rigidez
-    n = nodes.length*3
+    let n = nodes.length*3
 
     let matrizRigidezGlobal = []
 
-    //criado a mattriz de rigidez global base
+    //criado a mattriz de rigidez global base(matriz com valores nulos)
     for(let i=0; i<n; i++){
         matrizRigidezGlobal.push([])
         for(let j=0; j<n ; j++){
             matrizRigidezGlobal[i].push(0)
         }
     }
+
+    lines.forEach( line => {
+        //monta a matriz de rigidez local nos eixos globais
+        let mrLocalEixoGlobal = line.mntRigidezGlobal()
+        let vetorEspalhamento = line.mntVetorEspalhamento(nodes)
+
+        for(let i=0; i<6; i++){
+            for(let j=0; j<6; j++){
+                //o "-1" é por que os arrays começão de zero e a numeração das delocabilidades
+                // começam do 1
+                let iGlobal = vetorEspalhamento[i] - 1
+                let jGlobal = vetorEspalhamento[j] - 1
+                
+                matrizRigidezGlobal[iGlobal][jGlobal] += mrLocalEixoGlobal[i][j]
+            }
+        }    
+    });
+
+    return matrizRigidezGlobal
 }
+
+export {mntMatrizRigidezGlobal}
--- src/script.js
+++ src/script.js
@@ -1,8 +1,7 @@
 import {screen, ctx, drawLine, drawNode} from "./modules/canvas.js"
 import {Node, Line} from "./modules/classes.js"
 import * as menu from "./modules/menus.js"
-import * as engPerf from "./modules/coefEngastamento.js"
-import * as coefRigidez from "./modules/coefRigidez.js"
+import {mntMatrizRigidezGlobal} from "./modules/matrizRigidezGlobal.js"
 
 //seleções que guarda os nós criados
 let nodeForEdtion = document.getElementById('nodeForEdtion')
